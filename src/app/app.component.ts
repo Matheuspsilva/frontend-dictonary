0cf9a6d4ad33d274e3b1eed42f8ba04f61a6a200
Matheus <matheuspsilva29@gmail.com>
2022-06-07 20:06:17 -0300

    Criação Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { LoginServiceService } from './service/login-service.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontend-dictonary';
+
+  constructor(private router:Router){
+
+  }
+
+  ngOnInit(): void {
+    if (localStorage.getItem('token') == null) {
+      this.router.navigate(['login']);
+    }
+  }
+
+  public sair(){
+    localStorage.clear();
+    this.router.navigate(['login']);
+  }
 
 }
